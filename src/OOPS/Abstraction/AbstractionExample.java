d09188548a08b9ca2d58365c1ab59f8790c85cfd
AkashP20031 <akashp@ivoyant.com>
2025-06-20 15:19:19 +0530

    Abstraction

---
+++ src/OOPS/Abstraction/AbstractionExample.java
@@ -0,0 +1,10 @@
+package OOPS.Abstraction;
+
+public class AbstractionExample {
+    public static void main(String[] args) {
+        shape c = new Circle();
+        c.Draw();
+        shape s = new Square();
+        s.Draw();
+    }
+}
