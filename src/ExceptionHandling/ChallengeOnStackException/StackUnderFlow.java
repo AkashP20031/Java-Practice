80ddafb5b376c954ebd83e5ff25cf1a086febbd6
AkashP20031 <akashp@ivoyant.com>
2025-06-21 11:00:24 +0530

    Challenge on Stack Exception

---
+++ src/ExceptionHandling/ChallengeOnStackException/StackUnderFlow.java
@@ -0,0 +1,9 @@
+package ExceptionHandling.ChallengeOnStackException;
+
+public class StackUnderFlow extends Exception
+{
+    public String toString()
+    {
+        return " Stack Under Flow exception";
+    }
+}
