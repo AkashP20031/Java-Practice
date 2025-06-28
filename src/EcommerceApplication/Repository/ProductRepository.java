34ce46114693f437d8421e9fbd20339a32f78f08
AkashP20031 <akashp@ivoyant.com>
2025-06-28 22:19:36 +0530

    E-Commerce Application (28/06/25)

---
+++ src/EcommerceApplication/Repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package EcommerceApplication.Repository;
+
+import EcommerceApplication.Entity.Product;
+
+import java.util.HashMap;
+
+public class ProductRepository {
+    public static HashMap<Integer, Product> products = new HashMap<>();
+}
